8fe3892531e09a8bbd8ff59b87da9ff82b9b2433
heavylgf <863065649@qq.com>
2018-12-19 10:35:47 +0800

    firstlogin_playtogether_room_account1st

--- dc.etl.data2BI/spark/firstlogin_playtogether_room_account1st.py
+++ dc.etl.data2BI/spark/firstlogin_playtogether_room_account1st.py
@@ -38,146 +38,39 @@ def logic(start_date=DEFAULT_START_DATE, end_date=DEFAULT_END_DATE):
 
     hive_partition.dropPartition("bi.firstlogin_playtogether_room_account1st", "dt", start_date, end_date)
 
-    execute_sql_increase_table = '''
-            select t.uid, 
-                t.app, 
-                cast(t.date as int) date,
-                cast(t.time as int) time,
-                t.group, 
-                t.province, t.city, t.district, t.hardid, t.roomno, t.roomtype
-             from	
-            (select uid,
-                app, date, time, group, province, city, district, hardid, roomno, roomtype,
-                cast(concat(cast(date as  string), 
-                            case when length(time)=1 then concat('00000',cast(time as string)) 
-                                 when length(time)=2 then concat('0000',cast(time as string))
-                                 when length(time)=3 then concat('000',cast(time as string))
-                                 when length(time)=4 then concat('00',cast(time as string)) 
-                                 when length(time)=5 then concat('0',cast(time as string)) 
-                                 else cast(time as string) 
-                            end) as bigint) as date_time, 
-                row_number() over(partition by uid, app order by cast(concat(cast(date as  string),
-                case when length(time)=1 then concat('00000',cast(time as string)) 
-                     when length(time)=2 then concat('0000',cast(time as string))
-                     when length(time)=3 then concat('000',cast(time as string))
-                     when length(time)=4 then concat('00',cast(time as string)) 
-                     when length(time)=5 then concat('0',cast(time as string)) 
-                     else cast(time as string) 
-                end) as bigint) ) 
-                as rn
-                from ods.gsplaytogetherdb_gameactive
-                where app is not null and group is not null and group in (6, 66, 8, 88)
-            ) t
-            where rn = 1
-         '''
-
-    logger.warn(execute_sql_increase_table, 'sql')
-
-    increase_table = spark.sql(execute_sql_increase_table)
-
-    increase_table.write.mode('overwrite').format("orc").saveAsTable("stag.firstlogin_playtogether_room_account1st")
-
-    # 中间数据插入最终表
-    execute_sql_into_the_table = '''
-           select
-               t2.uid,
-               t2.app,
-               t2.date,
-               t2.time,
-               t2.group,
-               t2.province,
-               t2.city,
-               t2.district,
-               t2.hardid,
-               t2.roomno,
-               t2.roomtype,
-               t2.date as dt
-           from stag.firstlogin_playtogether_room_account1st t2
-       '''
-
-    logger.warn(execute_sql_into_the_table, 'sql')
-
-    the_end_table = spark.sql(execute_sql_into_the_table)
-
-    the_end_table.write.partitionBy("dt").mode('append').format("orc") \
-        .saveAsTable("bi.firstlogin_playtogether_room_account1st")
-
-    '''
-    将生成的数据增量插入至mongo中
-    '''
-    mongo = mongoExecute()
-
-    mongo.collectionAppend(the_end_table, "bi", "firstlogin_playtogether_room_account1st", start_date, end_date)
-
-    logger.warn("Job over", "banner")
-
     # execute_sql_increase_table = '''
-    #     select t2.uid,
-    #         t2.app,
-    #         t2.date,
-    #         t2.time,
-    #         t2.group,
-    #         t2.province,
-    #         t2.city,
-    #         t2.district,
-    #         t2.hardid,
-    #         t2.roomno,
-    #         t2.roomtype
-    #         from
-    #     (select uid,
-    #         app,
-    #         date,
-    #         time,
-    #         group,
-    #         province,
-    #         city,
-    #         district,
-    #         hardid,
-    #         roomno,
-    #         roomtype
-    #         from bi.firstlogin_playtogether_room_account1st
-    #     ) t1
-    #     right join
-    #     (select t.uid uid,
-    #             t.app app,
+    #         select t.uid,
+    #             t.app,
     #             cast(t.date as int) date,
     #             cast(t.time as int) time,
-    #             t.group group,
-    #             t.province province,
-    #             t.city city,
-    #             t.district district,
-    #             t.hardid hardid,
-    #             t.roomno roomno,
-    #             t.roomtype roomtype
-    #      from
-    #     (select uid,
-    #         app, date, time, group, province, city, district, hardid, roomno, roomtype,
-    #         cast(concat(cast(date as  string),
-    #                     case when length(time)=1 then concat('00000',cast(time as string))
-    #                          when length(time)=2 then concat('0000',cast(time as string))
-    #                          when length(time)=3 then concat('000',cast(time as string))
-    #                          when length(time)=4 then concat('00',cast(time as string))
-    #                          when length(time)=5 then concat('0',cast(time as string))
-    #                          else cast(time as string)
-    #                     end) as bigint) as date_time,
-    #         row_number() over(partition by uid, app order by cast(concat(cast(date as  string),
-    #         case when length(time)=1 then concat('00000',cast(time as string))
-    #              when length(time)=2 then concat('0000',cast(time as string))
-    #              when length(time)=3 then concat('000',cast(time as string))
-    #              when length(time)=4 then concat('00',cast(time as string))
-    #              when length(time)=5 then concat('0',cast(time as string))
-    #              else cast(time as string)
-    #         end) as bigint))
-    #         as rn
-    #         from ods.gsplaytogetherdb_gameactive
-    #         where app is not null and group is not null and group in (6, 66, 8, 88)
-    #     ) t
-    #     where rn = 1
-    #     ) t2
-    #     on t1.uid = t2.uid and t1.app = t2.app
-    #     where t1.uid is null
-    #  '''
-
+    #             t.group,
+    #             t.province, t.city, t.district, t.hardid, t.roomno, t.roomtype
+    #          from
+    #         (select uid,
+    #             app, date, time, group, province, city, district, hardid, roomno, roomtype,
+    #             cast(concat(cast(date as  string),
+    #                         case when length(time)=1 then concat('00000',cast(time as string))
+    #                              when length(time)=2 then concat('0000',cast(time as string))
+    #                              when length(time)=3 then concat('000',cast(time as string))
+    #                              when length(time)=4 then concat('00',cast(time as string))
+    #                              when length(time)=5 then concat('0',cast(time as string))
+    #                              else cast(time as string)
+    #                         end) as bigint) as date_time,
+    #             row_number() over(partition by uid, app order by cast(concat(cast(date as  string),
+    #             case when length(time)=1 then concat('00000',cast(time as string))
+    #                  when length(time)=2 then concat('0000',cast(time as string))
+    #                  when length(time)=3 then concat('000',cast(time as string))
+    #                  when length(time)=4 then concat('00',cast(time as string))
+    #                  when length(time)=5 then concat('0',cast(time as string))
+    #                  else cast(time as string)
+    #             end) as bigint) )
+    #             as rn
+    #             from ods.gsplaytogetherdb_gameactive
+    #             where app is not null and group is not null and group in (6, 66, 8, 88)
+    #         ) t
+    #         where rn = 1
+    #      '''
+    #
     # logger.warn(execute_sql_increase_table, 'sql')
     #
     # increase_table = spark.sql(execute_sql_increase_table)
@@ -186,21 +79,21 @@ def logic(start_date=DEFAULT_START_DATE, end_date=DEFAULT_END_DATE):
     #
     # # 中间数据插入最终表
     # execute_sql_into_the_table = '''
-    #     select
-    #         t2.uid,
-    #         t2.app,
-    #         t2.date,
-    #         t2.time,
-    #         t2.group,
-    #         t2.province,
-    #         t2.city,
-    #         t2.district,
-    #         t2.hardid,
-    #         t2.roomno,
-    #         t2.roomtype,
-    #         '20181217' as dt
-    #     from stag.firstlogin_playtogether_room_account1st t2
-    # '''
+    #        select
+    #            t2.uid,
+    #            t2.app,
+    #            t2.date,
+    #            t2.time,
+    #            t2.group,
+    #            t2.province,
+    #            t2.city,
+    #            t2.district,
+    #            t2.hardid,
+    #            t2.roomno,
+    #            t2.roomtype,
+    #            t2.date as dt
+    #        from stag.firstlogin_playtogether_room_account1st t2
+    #    '''
     #
     # logger.warn(execute_sql_into_the_table, 'sql')
     #
@@ -217,6 +110,113 @@ def logic(start_date=DEFAULT_START_DATE, end_date=DEFAULT_END_DATE):
     # mongo.collectionAppend(the_end_table, "bi", "firstlogin_playtogether_room_account1st", start_date, end_date)
     #
     # logger.warn("Job over", "banner")
+
+    execute_sql_increase_table = '''
+        select t2.uid,
+            t2.app,
+            t2.date,
+            t2.time,
+            t2.group,
+            t2.province,
+            t2.city,
+            t2.district,
+            t2.hardid,
+            t2.roomno,
+            t2.roomtype
+            from
+        (select uid,
+            app,
+            date,
+            time,
+            group,
+            province,
+            city,
+            district,
+            hardid,
+            roomno,
+            roomtype
+            from bi.firstlogin_playtogether_room_account1st
+        ) t1
+        right join
+        (select t.uid uid,
+                t.app app,
+                cast(t.date as int) date,
+                cast(t.time as int) time,
+                t.group group,
+                t.province province,
+                t.city city,
+                t.district district,
+                t.hardid hardid,
+                t.roomno roomno,
+                t.roomtype roomtype
+         from
+        (select uid,
+            app, date, time, group, province, city, district, hardid, roomno, roomtype,
+            cast(concat(cast(date as  string),
+                        case when length(time)=1 then concat('00000',cast(time as string))
+                             when length(time)=2 then concat('0000',cast(time as string))
+                             when length(time)=3 then concat('000',cast(time as string))
+                             when length(time)=4 then concat('00',cast(time as string))
+                             when length(time)=5 then concat('0',cast(time as string))
+                             else cast(time as string)
+                        end) as bigint) as date_time,
+            row_number() over(partition by uid, app order by cast(concat(cast(date as  string),
+            case when length(time)=1 then concat('00000',cast(time as string))
+                 when length(time)=2 then concat('0000',cast(time as string))
+                 when length(time)=3 then concat('000',cast(time as string))
+                 when length(time)=4 then concat('00',cast(time as string))
+                 when length(time)=5 then concat('0',cast(time as string))
+                 else cast(time as string)
+            end) as bigint))
+            as rn
+            from ods.gsplaytogetherdb_gameactive
+            where app is not null and group is not null and group in (6, 66, 8, 88)
+        ) t
+        where rn = 1
+        ) t2
+        on t1.uid = t2.uid and t1.app = t2.app
+        where t1.uid is null
+     '''
+
+    logger.warn(execute_sql_increase_table, 'sql')
+
+    increase_table = spark.sql(execute_sql_increase_table)
+
+    increase_table.write.mode('overwrite').format("orc").saveAsTable("stag.firstlogin_playtogether_room_account1st")
+
+    # 中间数据插入最终表
+    execute_sql_into_the_table = '''
+        select
+            t2.uid,
+            t2.app,
+            t2.date,
+            t2.time,
+            t2.group,
+            t2.province,
+            t2.city,
+            t2.district,
+            t2.hardid,
+            t2.roomno,
+            t2.roomtype,
+            t2.date as dt
+        from stag.firstlogin_playtogether_room_account1st t2
+    '''
+
+    logger.warn(execute_sql_into_the_table, 'sql')
+
+    the_end_table = spark.sql(execute_sql_into_the_table)
+
+    the_end_table.write.partitionBy("dt").mode('append').format("orc") \
+        .saveAsTable("bi.firstlogin_playtogether_room_account1st")
+
+    '''
+    将生成的数据增量插入至mongo中
+    '''
+    mongo = mongoExecute()
+
+    mongo.collectionAppend(the_end_table, "bi", "firstlogin_playtogether_room_account1st", start_date, end_date)
+
+    logger.warn("Job over", "banner")
 
 if __name__ == "__main__":
     argv = arguments(sys.argv)
